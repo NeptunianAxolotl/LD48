d9622c562d8d48ac0859765314c3c855740da054
David <liddelow165@gmail.com>
2021-04-24 20:46:03 +1000

    rock single

--- resources/defs/rock.lua
+++ resources/defs/rock.lua
@@ -1,6 +1,6 @@
 return {
 	form = "image", -- image, sound or animation
-	file = "resources/images/rock.png",
+	file = "resources/images/rock_single.png",
 	xScale = 32/400,
 	yScale = 32/400,
 	xOffset = 0,
