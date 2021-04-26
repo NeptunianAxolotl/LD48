dda6e7ce15bdc27441fdc91cf41e5ea8897122ab
GoogleFrog <googl3frog@gmail.com>
2021-04-27 01:32:56 +1000

    Added name and icon.

--- conf.lua
+++ conf.lua
@@ -1,10 +1,10 @@
 function love.conf(t)
-	t.window.title = "LD48"
+	t.window.title = "Polyominio Prospector"
 	t.window.width = 1120
 	t.window.height = 832
 	--t.window.fullscreen = true -- Do not fullscreen since we lack an exit button.
 	t.window.resizable = true
-	--t.window.icon = "resources/images/hat.png"
+	t.window.icon = "resources/images/pieceblocks/icon.png"
 
 	t.modules.joystick = false
 	--t.window.fullscreen = true 
